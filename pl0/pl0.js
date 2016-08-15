ec52a60e24c977d14560c1ce00be01d26b9a40a6
lesguillemets <lesguillemets@users.noreply.github.com>
2016-08-16 00:12:14 +0900

    initial example

--- pl0/pl0.js
+++ pl0/pl0.js
@@ -1,5 +1,12 @@
 function main(){
   var cnv = new fabric.Canvas('world');
+  var rect = new fabric.Rect( {
+    'left' : 100,
+    'top' : 100,
+    'fill' : 'blue',
+    'width' : 20,
+    'height' : 20});
+  cnv.add(rect);
 }
 
 window.addEventListener('load', main)
