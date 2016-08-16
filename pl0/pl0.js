d921d5da14e4ae1dda25d14573ab82167e21ffbd
lesguillemets <lesguillemets@users.noreply.github.com>
2016-08-16 15:12:36 +0900

    fmfm

--- pl0/pl0.js
+++ pl0/pl0.js
@@ -1,12 +1,18 @@
 function main(){
   var cnv = new fabric.Canvas('world');
   var rect = new fabric.Rect( {
-    'left' : 100,
-    'top' : 100,
-    'fill' : 'blue',
-    'width' : 20,
-    'height' : 20});
+    left : 100,
+    top : 100,
+    fill : 'blue',
+    width : 20,
+    height : 20,
+    angle : 45
+  });
   cnv.add(rect);
+  alert('hi');
+  rect.set({left:150});
+  cnv.renderAll();
+  console.log(cnv.getObjects());
 }
 
-window.addEventListener('load', main)
+window.addEventListener('load', main);
